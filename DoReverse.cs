aa544bef5f4e6dd910c20b50ae756367a897046c
DXinter <58430974+DXinter@users.noreply.github.com>
2019-12-09 18:18:13 +0300

    Add files via upload

---
+++ DoReverse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reverse
+{
+    class DoReverse
+    {
+        public string Reverse(string someString)
+        {
+            char[] someArray = someString.ToCharArray();
+            Array.Reverse(someArray);
+            string result = new string(someArray);
+            return result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Reverse
+{
+    /*
+     *функциия Reverse(string input), которая возвращает обратную строку (Reverse("ABCD") -> "DCBA") 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DoReverse reverse = new DoReverse();
+            Console.WriteLine("Введите строку: ");
+            string someString = Console.ReadLine();
+            string result = reverse.Reverse(someString);
+            Console.WriteLine($"{result}");
+           
+        }
+    }
+}
